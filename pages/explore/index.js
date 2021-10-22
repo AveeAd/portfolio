a9c4d64551e2a3cf518b93b299046f281064cf8c
Kumar Adhikari / Avee <kumarad556@gmail.com>
2021-10-22 21:39:01 +0545

    explore page intro

---
+++ pages/explore/index.js
@@ -0,0 +1,12 @@
+import Header from "../../components/Header/Header";
+
+const index = () => {
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Header />
+      <p className="text-black font-medium text-lg">On Progress</p>
+    </main>
+  );
+};
+
+export default index;
