f828d7a4c6301dbc45847774df36ad374f238bbd
Kumar Adhikari / Avee <kumarad556@gmail.com>
2021-10-22 20:04:42 +0545

    first commit

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -20,7 +20,7 @@ const Footer = () => {
             className="p-2 w-64 md:w-80 rounded-xl text-black focus:border-2 focus:border-indigo-600"
             placeholder="Feedback"
           />
-          <button className="btn bg-indigo-600 border-white border-2 my-1 py-2 px-8 rounded-xl font-medium hover:bg-indigo-500">
+          <button className="btn bg-indigo-600 border-white border-2 my-1 py-2 px-4 md:px-8 rounded-xl font-medium hover:bg-indigo-500">
             Submit
           </button>
         </div>
